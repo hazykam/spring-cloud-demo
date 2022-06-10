96d8a7d8d14dddbe96f8aaa80aba2ffe869881f5
graytang <graytang.wing@hotmail.com>
2022-06-10 15:21:09 +0800

    Learn server-side and client-side support for externalized configuration in a distributed system.

---
+++ config-client/src/main/java/com/example/controller/ConfigClientController.java
@@ -0,0 +1,28 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName ConfigClientController
+ * @Description TODO
+ * @Author Gray
+ * @Date 2022/6/8 14:54
+ * @Version 1.0
+ **/
+@RefreshScope
+@RestController
+@RequestMapping("/config")
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/info")
+    public String getInfo() {
+        return configInfo;
+    }
+
+}
